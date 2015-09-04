0beeb38027ac7b04ffaef9105d4cef52c86a8f60
Yunying <yunyingt@usc.edu>
2015-09-04 12:03:10 -0700

    Single month filter finish

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'pages#index'
+   root 'contracts#index'
    Rails.application.routes.draw do
     get "/pages/index" => "pages#index"
    end
